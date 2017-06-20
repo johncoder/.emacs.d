5bf31ad45e0ca936432a9f8532faa7d78e2713e0
John Nelson <jnelson@johncoder.com>
2017-06-19 23:29:48 -0400

    Initial commit

---
+++ init.el
@@ -0,0 +1,21 @@
+(package-initialize)
+
+(require 'org)
+(require 'ob-tangle)
+
+(setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
+(org-babel-load-file (expand-file-name "loader.org" init-dir))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (handlebars-mode counsel bm magit atom-one-dark-theme spaceline-all-the-icons all-the-icons monochrome-theme use-package base16-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
