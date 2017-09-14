491360cfdd78db16ca8f72a84d75e253f6d41625
John Nelson <johnnelson@AV-JNELSON-MBP.local>
2017-09-14 11:28:58 -0700

    Adds rest-client and ob-restclient

--- init.el
+++ init.el
@@ -12,7 +12,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (csharp-mode handlebars-mode counsel bm magit atom-one-dark-theme spaceline-all-the-icons all-the-icons monochrome-theme use-package base16-theme))))
+    (ob-restclient rest-client csharp-mode handlebars-mode counsel bm magit atom-one-dark-theme spaceline-all-the-icons all-the-icons monochrome-theme use-package base16-theme)))
+ '(projectile-mode t nil (projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
