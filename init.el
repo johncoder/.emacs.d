4e13f4bdeecc7f7cb86952c6f687abf68eb51e6f
John Nelson <john@abstract.com>
2019-06-06 16:50:04 -0400

    Add support for work.el
    Hopefully this never conflicts :joy:

--- init.el
+++ init.el
@@ -5,3 +5,6 @@
 
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
 (org-babel-load-file (expand-file-name "loader.org" init-dir))
+
+(when (file-exists-p "./work.el")
+  (load-file "./work.el"))
