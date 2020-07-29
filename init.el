22ae3c833692bc9611ef28672fedfa75be55d461
John Nelson <john@abstract.com>
2020-07-29 09:17:09 -0400

    rename loader.org -> README.org; update .gitignore

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (require 'ob-tangle)
 
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
-(org-babel-load-file (expand-file-name "loader.org" init-dir))
+(org-babel-load-file (expand-file-name "README.org" init-dir))
 
 (when (file-exists-p "./work.el")
   (load-file "./work.el"))
