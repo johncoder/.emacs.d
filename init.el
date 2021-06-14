625daa0938404f6596c71e1666b081c847c1a3ec
John Nelson <johnnelson@johns-mbp.home>
2021-06-14 10:17:13 -0400

    sql connections, work.el, font!

--- init.el
+++ init.el
@@ -1,10 +1,20 @@
+;;; init.el --- johncoder's Emacs configuration -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
 (package-initialize)
 
 (require 'org)
 (require 'ob-tangle)
 
+(defvar init-dir nil)
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
 (org-babel-load-file (expand-file-name "README.org" init-dir))
 
-(when (file-exists-p "./work.el")
-  (load-file "./work.el"))
+(message "loading work.el...")
+(load-file (expand-file-name "work.el" init-dir))
+(message "loaded work.el!")
+
+;; (when (file-exists-p "work.el")
+;;   (message "Loading work.el...")
+;;   (load-file "work.el"))
+;;; init.el ends here
